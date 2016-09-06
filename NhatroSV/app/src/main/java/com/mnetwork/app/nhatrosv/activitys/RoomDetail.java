05b339ba0ec3e525ff03e246e5aab75f3204efb7
VanThanh <phanvanthanh.mrt@gmail.com>
2016-09-06 21:14:18 +0700

    Them activity RoomDetail

---
+++ NhatroSV/app/src/main/java/com/mnetwork/app/nhatrosv/activitys/RoomDetail.java
@@ -0,0 +1,15 @@
+package com.mnetwork.app.nhatrosv.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mnetwork.app.nhatrosv.R;
+
+public class RoomDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_detail);
+    }
+}
